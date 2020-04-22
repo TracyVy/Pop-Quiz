6959635f30a8d7a34a84d2d2f4d55817fffa7295
Tracy Vy <tracyvy88@gmail.com>
2020-04-22 14:10:37 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// #1 Create a timer for 90 seconds, upper right corner
+// #2 Create a button, "View Highscores", and show local storage's memory of recent scores and rank.
+// #3 Create a Quiz
+
+// SET timeInterval
+// var timeEl = document.querySelector(".time");
+// var endEl = document.querySelector("#end");
+// var secondsLeft = 90;
+
+// function setTime() {
+//     var timeInterval = set Interval(function () {
+//         secondsLeft--;
+//         timeEl.textContent = secondsLeft + " seconds left.";
+
+//         if (secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//         }
+//     }, 1000);
+//     function sendMessage();
+// }
+
+// function sendMessage() {
+//     timeEl.textContent = "";
+//     endEl.appendChild();
+// }
+
+// setTime();
